c4071f6d117bca5387b609e3934134339c2ad7c2
tomassundvall <tomas.sundvall@gmail.com>
2017-08-16 17:11:20 +0200

    Added install ansible fle

---
+++ install_ansible.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if ! command -v ansible > /dev/null; then
+    echo "Install Ansible dependencies and Git"
+    sudo apt-get install -y -qq git python-yaml python-pip build-essential libssl-dev libffi-dev python-dev
+    pip install --upgrade pip
+    pip install paramiko pyyaml jinja2 markupsafe
+    pip install ansible
+fi
